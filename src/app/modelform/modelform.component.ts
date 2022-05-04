6d4fa56abfdd802427cd35580cea049cf6fec8f1
RaquelYang <lorehmmoreh007@gmail.com>
2022-05-04 14:26:32 +0800

    [ Update ] add model form component

---
+++ src/app/modelform/modelform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modelform',
+  templateUrl: './modelform.component.html',
+  styleUrls: ['./modelform.component.scss']
+})
+export class ModelformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
