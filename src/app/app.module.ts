0bd52a87b4a58e3185ace670c7ba06b6950bf7c2
RaquelYang <lorehmmoreh007@gmail.com>
2022-05-04 14:41:24 +0800

    [ Update ] import ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateformComponent } from './templateform/templateform.component';
+import { ModelformComponent } from './modelform/modelform.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateformComponent
+    TemplateformComponent,
+    ModelformComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/templateform/templateform.component.ts
+++ src/app/templateform/templateform.component.ts
@@ -11,5 +11,4 @@ export class TemplateformComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
