479ed16c5088dcf49f86457b3767b5e0f1d96d18
RaquelYang <lorehmmoreh007@gmail.com>
2022-05-04 13:54:50 +0800

    [ Update ] 使用 Angular 內建屬性更改 css

--- src/app/templateform/templateform.component.ts
+++ src/app/templateform/templateform.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./templateform.component.scss']
 })
 export class TemplateformComponent implements OnInit {
-  email:string = "abc@abc.com"
+  email: string = "abc@abc.com"
   constructor() { }
 
   ngOnInit(): void {
