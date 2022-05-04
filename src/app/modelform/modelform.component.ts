3ef2d4df09ec1a030c0a31e3dc5e260043d72358
RaquelYang <lorehmmoreh007@gmail.com>
2022-05-04 14:49:59 +0800

    [ Update ] 初始化表單 FormBuilder

--- src/app/modelform/modelform.component.ts
+++ src/app/modelform/modelform.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,Validators,FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-modelform',
   templateUrl: './modelform.component.html',
@@ -7,7 +7,23 @@ import { FormBuilder,Validators,FormGroup } from '@angular/forms';
 })
 export class ModelformComponent implements OnInit {
   form;
-  constructor() { }
+  constructor(private _fb: FormBuilder) {
+    this.form = this._fb.group({
+      firstName: '',
+      nickName: '',
+      email: '',
+      phone: '',
+      birthday: '',
+      interest: this._fb.group({
+        movie: '',
+        music: '',
+        technology: '',
+        sports: '',
+        games: ''
+      }),
+      sex: ''
+    });
+  }
 
   ngOnInit(): void {
   }
