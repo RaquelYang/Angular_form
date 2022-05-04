5c7562296c81c072e71e59c5233f55baa3c7269a
RaquelYang <lorehmmoreh007@gmail.com>
2022-05-04 14:44:45 +0800

    [ Update ] 引入 template 模板

--- src/app/modelform/modelform.component.ts
+++ src/app/modelform/modelform.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder,Validators,FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-modelform',
   templateUrl: './modelform.component.html',
   styleUrls: ['./modelform.component.scss']
 })
 export class ModelformComponent implements OnInit {
-
+  form;
   constructor() { }
 
   ngOnInit(): void {
